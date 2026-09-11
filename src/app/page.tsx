f867bf43ce7ba9704761f5b11c048cdfffa147ae
Rishabh Chauhan <rshbhchauhan@gmail.com>
2026-09-11 18:03:05 +0530

    adding betterauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
+import { headers } from "next/headers";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 import { LatestPost } from "~/app/_components/post";
-import { auth } from "~/server/auth";
+import { auth } from "~/server/better-auth";
+import { getSession } from "~/server/better-auth/server";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await auth();
+  const session = await getSession();
 
-  if (session?.user) {
+  if (session) {
     void api.post.getLatest.prefetch();
   }
 
@@ -52,12 +55,43 @@ export default async function Home() {
               <p className="text-center text-2xl text-white">
                 {session && <span>Logged in as {session.user?.name}</span>}
               </p>
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
+              {!session ? (
+                <form>
+                  <button
+                    className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+                    formAction={async () => {
+                      "use server";
+                      const res = await auth.api.signInSocial({
+                        body: {
+                          provider: "github",
+                          callbackURL: "/",
+                        },
+                      });
+                      if (!res.url) {
+                        throw new Error("No URL returned from signInSocial");
+                      }
+                      redirect(res.url);
+                    }}
+                  >
+                    Sign in with Github
+                  </button>
+                </form>
+              ) : (
+                <form>
+                  <button
+                    className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+                    formAction={async () => {
+                      "use server";
+                      await auth.api.signOut({
+                        headers: await headers(),
+                      });
+                      redirect("/");
+                    }}
+                  >
+                    Sign out
+                  </button>
+                </form>
+              )}
             </div>
           </div>
 
